ea29a9d107def62f5618e5f952fdb23993db3e64
hannanmiyaji <hannanmiyaji729@gmail.com>
2024-09-26 22:14:41 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+class Book{
+  String? name;
+  String? author;
+  String? year;
+
+}
+
+void main(){
+  Book st = Book();
+  st.name="Baburnama";
+  st.author= "Babur";
+  st.year = "1931";
+  
+  print(st.name);
+  print(st.author);
+  print(st.year);
+
+}
